85be25ccde4e5f877c0b4d350d71a8f698385f66
maulana isbad rofiqi <147914104+maulana-isbad-rofiqi@users.noreply.github.com>
2025-12-03 09:51:14 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'itsbad-dl-v2';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Orbitron:wght@500;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(res => res || fetch(event.request))
+    );
+});
